9bcf836ef7391a0f24356ac4ece37bc91b462d96
andro0045 <cool.fufel@yandex.ru>
2024-11-24 22:08:01 +0300

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -1,1 +1,10 @@
 cp padavan_logo.png padavan-ng/trunk/user/www/n56u_ribbon_fixed/bootstrap/img
+cp loading_bg.png padavan-ng/trunk/user/www/n56u_ribbon_fixed/bootstrap/img
+cp itoggle.png padavan-ng/trunk/user/www/n56u_ribbon_fixed/bootstrap/img
+cp glyphicons-halflings-white.png padavan-ng/trunk/user/www/n56u_ribbon_fixed/bootstrap/img
+cp glyphicons-halflings.png padavan-ng/trunk/user/www/n56u_ribbon_fixed/bootstrap/img
+cp dark-bg.jpg padavan-ng/trunk/user/www/n56u_ribbon_fixed/bootstrap/img
+cp arrow-right.png padavan-ng/trunk/user/www/n56u_ribbon_fixed/bootstrap/img
+cp ajax-loader.gif padavan-ng/trunk/user/www/n56u_ribbon_fixed/bootstrap/img
+cp engage.itoggle.css padavan-ng/trunk/user/www/n56u_ribbon_fixed/bootstrap/css
+cp main.css padavan-ng/trunk/user/www/n56u_ribbon_fixed/bootstrap/css
