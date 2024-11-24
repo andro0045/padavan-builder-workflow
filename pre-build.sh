102b2b39c6a608eaaea479b6a4c121cef8725352
andro0045 <cool.fufel@yandex.ru>
2024-11-24 20:53:35 +0300

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -1,1 +1,1 @@
-cp https://github.com/andro0045/padavan-builder-workflow/blob/581f8958a2067b817d03c70ee516a5ece014b181/padavan_logo.png  padavan-ng/trunk/user/www/n56u_ribbon_fixed/bootstrap/img
+cp padavan-builder-workflow/padavan_logo.png padavan-ng/trunk/user/www/n56u_ribbon_fixed/bootstrap/img
