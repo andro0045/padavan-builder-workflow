7bd81e664b493fa3f129147976f27f17e66d8a44
andro0045 <cool.fufel@yandex.ru>
2024-11-25 09:14:45 +0300

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -8,8 +8,3 @@ cp arrow-right.png padavan-ng/trunk/user/www/n56u_ribbon_fixed/bootstrap/img
 cp ajax-loader.gif padavan-ng/trunk/user/www/n56u_ribbon_fixed/bootstrap/img
 cp engage.itoggle.css padavan-ng/trunk/user/www/n56u_ribbon_fixed/bootstrap/css
 cp main.css padavan-ng/trunk/user/www/n56u_ribbon_fixed/bootstrap/css
-
-### Смена часового пояса
-ln -snf /usr/share/zoneinfo/Europe/Kyiv /etc/localtime
-apt update
-DEBIAN_FRONTEND=noninteractive apt install tzdata -y
