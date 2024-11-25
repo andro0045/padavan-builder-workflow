36ff958d16e0afa103601acf441aac29013972c9
andro0045 <cool.fufel@yandex.ru>
2024-11-25 18:12:16 +0300

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -1,1 +1,7 @@
-cp padavan_logo.png loading_bg.png itoggle.png glyphicons-halflings-white.png glyphicons-halflings.png dark-bg.jpg arrow-right.png ajax-loader.gif padavan-ng/trunk/user/www/n56u_ribbon_fixed/bootstrap/img && cp engage.itoggle.css main.css padavan-ng/trunk/user/www/n56u_ribbon_fixed/bootstrap/css
+# Копирование изображений в директорию img
+cp padavan_logo.png loading_bg.png itoggle.png glyphicons-halflings-white.png \
+   glyphicons-halflings.png dark-bg.jpg arrow-right.png ajax-loader.gif \
+   padavan-ng/trunk/user/www/n56u_ribbon_fixed/bootstrap/img/
+
+# Копирование стилей CSS в директорию css
+cp engage.itoggle.css main.css padavan-ng/trunk/user/www/n56u_ribbon_fixed/bootstrap/css/
