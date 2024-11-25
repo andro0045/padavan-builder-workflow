66ba6a8278cd26d73c5900ed48bd713aae3baa83
andro0045 <cool.fufel@yandex.ru>
2024-11-25 19:21:13 +0300

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -1,13 +1,7 @@
-#!/bin/bash
-
-# Определяем директории назначения
-IMG_DIR="padavan-ng/trunk/user/www/n56u_ribbon_fixed/bootstrap/img"
-CSS_DIR="padavan-ng/trunk/user/www/n56u_ribbon_fixed/bootstrap/css"
-
 # Копирование изображений в директорию img
-cp padavan_logo.png loading_bg.png itoggle.png \
-   glyphicons-halflings-{white,.png} dark-bg.jpg arrow-right.png ajax-loader.gif \
-   "$IMG_DIR/"
+cp padavan_logo.png loading_bg.png itoggle.png glyphicons-halflings-white.png \
+   glyphicons-halflings.png dark-bg.jpg arrow-right.png ajax-loader.gif \
+   padavan-ng/trunk/user/www/n56u_ribbon_fixed/bootstrap/img/
 
 # Копирование стилей CSS в директорию css
-cp engage.itoggle.css main.css "$CSS_DIR/"
+cp engage.itoggle.css main.css padavan-ng/trunk/user/www/n56u_ribbon_fixed/bootstrap/css/
