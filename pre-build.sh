d74a988bf8f3b630adddcfe47b9b431a1120d2fc
andro <cool.fufel@yandex.ru>
2025-01-01 07:48:38 +0300

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -5,3 +5,6 @@ cp padavan_logo.png loading_bg.png itoggle.png glyphicons-halflings-white.png \
 
 # Копирование CSS файлов в директорию css
 cp engage.itoggle.css main.css padavan-ng/trunk/user/www/n56u_ribbon_fixed/bootstrap/css/
+
+# Отключение log wireguard debug
+sed -i 's/func_enable_kernel_param "CONFIG_WIREGUARD_DEBUG"/func_disable_kernel_param "CONFIG_WIREGUARD_DEBUG"/' padavan-ng/trunk/build_firmware.sh
