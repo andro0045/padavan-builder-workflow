fa6cdc99bfe11e0573cd3e00412626dfa4654ad2
andro <cool.fufel@yandex.ru>
2025-11-22 20:41:43 +0300

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -8,19 +8,11 @@ cp engage.itoggle.css main.css padavan-ng/trunk/user/www/n56u_ribbon_fixed/boots
 
 
 
-#!/bin/bash
-set -e
+TRUNK="$(pwd)/trunk"
+ZAPRET="/tmp/zapret"
 
-BASE=padavan-ng/trunk/user
-git clone https://github.com/bol-van/zapret.git "$BASE/nfqws-new"
-TAG=$(git ls-remote --tags https://github.com/bol-van/zapret.git | awk -F'/' '{print $3}' | grep -E '^v[0-9]+' | sort -V | tail -n1)
-git -C "$BASE/nfqws-new" checkout "$TAG"
-rm -rf "$BASE/nfqws-new/.git" "$BASE/nfqws-new/.github" "$BASE/nfqws-new/tools"
-cp -f "$BASE/nfqws/Makefile" "$BASE/nfqws-new/Makefile" 2>/dev/null || true
-cp -f "$BASE/nfqws/zapret.sh" "$BASE/nfqws-new/zapret.sh" 2>/dev/null || true
-rm -rf "$BASE/nfqws"
-mv "$BASE/nfqws-new" "$BASE/nfqws"
-find "$BASE/nfqws" -type f -name Makefile -exec sed -i 's/-flto[^ ]*//g' {} +
-export CFLAGS="${CFLAGS//-flto/}"
-export CXXFLAGS="${CXXFLAGS//-flto/}"
-echo ">>> nfqws обновлён до $TAG, LTO отключен"
+rm -rf "$TRUNK/user/nfqws"
+[ -d "$ZAPRET" ] || git clone https://github.com/bol-van/zapret.git "$ZAPRET"
+cd "$ZAPRET" && git fetch --tags && git checkout v72.2
+cp -r nfqws "$TRUNK/user/"
+rm -rf "$ZAPRET"
