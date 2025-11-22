c4e5ac4d9a623ec15d94cd6155eddee820fa02ac
andro <cool.fufel@yandex.ru>
2025-11-22 19:27:32 +0300

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -6,10 +6,17 @@ cp padavan_logo.png loading_bg.png itoggle.png glyphicons-halflings-white.png \
 # Копирование CSS файлов в директорию css
 cp engage.itoggle.css main.css padavan-ng/trunk/user/www/n56u_ribbon_fixed/bootstrap/css/
 
+
 set -e
 cd padavan-ng/trunk/user
-rm -rf nfqws
-git clone https://github.com/bol-van/zapret.git nfqws
-cd nfqws
-git checkout v72.2
+git clone https://github.com/bol-van/zapret.git nfqws-new
+cd nfqws-new
+LATEST_TAG=$(git ls-remote --tags https://github.com/bol-van/zapret.git | awk -F'/' '{print $3}' | grep -E '^v[0-9]+' | sort -V | tail -n1)
+git checkout "$LATEST_TAG"
 rm -rf .git .github tools
+cp -f ../nfqws/Makefile ./Makefile 2>/dev/null || true
+cp -f ../nfqws/zapret.sh ./zapret.sh 2>/dev/null || true
+cd ..
+rm -rf nfqws
+mv nfqws-new nfqws
+echo ">>> nfqws обновлён до $LATEST_TAG"
