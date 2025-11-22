cf0bc124fbe0661a428a9caa84f7373031e511d4
andro <cool.fufel@yandex.ru>
2025-11-22 21:36:00 +0300

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -7,28 +7,31 @@ cp padavan_logo.png loading_bg.png itoggle.png glyphicons-halflings-white.png \
 cp engage.itoggle.css main.css padavan-ng/trunk/user/www/n56u_ribbon_fixed/bootstrap/css/
 
 
-
 set -e
 
 cd /tmp
 rm -rf zapret-v72.2*
 wget -q https://github.com/bol-van/zapret/archive/refs/tags/v72.2.tar.gz
 tar -xzf v72.2.tar.gz
-cd zapret-v72.2
 
 rm -rf "$PADAVAN_BUILD_PATH/trunk/user/nfqws"/*
-cp -a nfq/* shared/* "$PADAVAN_BUILD_PATH/trunk/user/nfqws/"
+cp -a zapret-v72.2/nfq/* zapret-v72.2/shared/* "$PADAVAN_BUILD_PATH/trunk/user/nfqws/"
 
 cat > "$PADAVAN_BUILD_PATH/trunk/user/nfqws/Makefile" <<'EOF'
 ifeq ($(CONFIG_FIRMWARE_INCLUDE_NFQWS),y)
+
 TARGETS += nfqws
 NFQWS_OBJ = nfqws.o ws.o opt.o util.o ipq.o list.o
+
 $(obj)/nfqws: $(addprefix $(obj)/,$(NFQWS_OBJ))
 	$(CC) $(CFLAGS) -o $@ $^ $(LDFLAGS) -lnetfilter_queue -lnfnetlink -lmnl
+
 romfs:
 	$(ROMFSINST) $(obj)/nfqws /usr/sbin/nfqws
 	$(ROMFSINST) -s /usr/sbin/nfqws /tmp/zapret
+
 endif
+
 CFLAGS += -DNFQWS_VERSION=\"v72.2\"
 EOF
 
