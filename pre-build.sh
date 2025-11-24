00f5866ea80595bed5390b61215919c01d7f6024
andro <cool.fufel@yandex.ru>
2025-11-24 14:31:43 +0300

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -5,3 +5,15 @@ cp padavan_logo.png loading_bg.png itoggle.png glyphicons-halflings-white.png \
 
 # Копирование CSS файлов в директорию css
 cp engage.itoggle.css main.css padavan-ng/trunk/user/www/n56u_ribbon_fixed/bootstrap/css/
+
+NFQWS_DIR="padavan-ng/trunk/user/nfqws"
+ZIP_FILE="nfqws.zip"
+
+echo "==> Очистка каталога $NFQWS_DIR"
+rm -rf "$NFQWS_DIR"
+mkdir -p "$NFQWS_DIR"
+
+echo "==> Распаковка $ZIP_FILE в $NFQWS_DIR"
+unzip -jo "$ZIP_FILE" -d "$NFQWS_DIR"
+
+echo "==> Готово"
