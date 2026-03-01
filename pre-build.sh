2a5d9a5857f41a66a78e2201f2a8eb84f1434f36
andro <cool.fufel@yandex.ru>
2026-03-01 16:55:23 +0300

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -16,3 +16,9 @@ cp engage.itoggle.css main.css padavan-ng/trunk/user/www/n56u_ribbon_fixed/boots
 rm -rf padavan-ng/trunk/user/nfqws
 mkdir -p padavan-ng/trunk/user/nfqws
 unzip -o nfqws.zip -d padavan-ng/trunk/user/nfqws
+
+echo "Disable LTO"
+# убрать LTO-флаги
+find . -type f -exec sed -i 's/-flto//g' {} +
+find . -type f -exec sed -i 's/-fuse-linker-plugin//g' {} +
+echo "Done"
